c09abccc18dd31909fc0f93f06d2f36e0260c0e5
mistimalaviya <mistiswaminarayan@gmail.com>
2024-11-22 14:16:48 +0530

    react js

---
+++ day4/src/App.jsx
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react'
+import Counter from './Counter'
+
+
+function App() {
+  let[state,setState]=useState(0) 
+  function inc(){
+    setState(state+1)
+  }
+  function des(){
+    setState(state-1)
+    if(state <= 0 ){
+      setState(0) 
+    }
+  }
+  function restart(){
+    setState(state=0)
+  }
+  return (
+    <div className="App">
+      <p>{state}</p>
+      <div className='count'>
+      <h1>Counter</h1>
+      <div className='btn'>
+      <button onClick={des} >-</button>
+      <Counter a={state}/>
+      <button onClick={inc}>+</button>
+      
+      </div>
+      <button className='restart' onClick={restart}>ReStart</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
